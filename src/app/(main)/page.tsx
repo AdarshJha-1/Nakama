0a3fe6f69bac04bf7c96232e9085a6280a652194
Adarsh Jha <adarsh2005j@gmail.com>
2025-07-29 22:21:36 +0530

    fix: auth issue redirect, need proper auth checking flow

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,7 @@
 import AddPost from "@/components/AddPost";
-import LeftSidebar from "@/components/LeftSidebar";
-import Image from "next/image";
 
 export default function Home() {
+
   return (
     <div className="w-full pt-5">
       <div className="w-full ">
--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,20 +1,21 @@
+"use client"
+
 import { auth, currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Tiptap from "./Editor/Tiptap";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
-export default async function AddPost() {
+export default function AddPost() {
 
-    const { userId } = await auth()
-    if (!userId) {
-        throw new Error("Not Authenticated")
-    }
-    const user = await currentUser()
+    const router = useRouter()
+    const { user, isSignedIn, isLoaded } = useUser()
 
     return (
         <div className="max-w-xl flex gap-3 items-start justify-start ml-5">
             <div className="rounded-full overflow-hidden">
-                <Image src={user?.imageUrl as string} alt="avatar" width={"55"} height={"55"} />
-            </div>
+                {user?.imageUrl && <Image src={user?.imageUrl as string} alt="avatar" width={"55"} height={"55"} />
+                }            </div>
             <Tiptap />
         </div>
     )
