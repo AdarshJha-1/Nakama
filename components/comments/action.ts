d8560d945ceee630aa3bcabbc4ea8f5bc08cbf3f
Adarsh Jha <adarsh2005j@gmail.com>
2026-05-18 22:33:59 +0530

    Add: Comment notification

--- components/comments/action.ts
+++ components/comments/action.ts
@@ -2,7 +2,7 @@
 
 import { db } from "@/db/drizzle"
 import { userFromDB } from "@/db/helper"
-import { comments, user } from "@/db/schema"
+import { comments, notification, user } from "@/db/schema"
 import { getServerSession } from "@/lib/getServerSession"
 import { CommentDTO, PostDTO } from "@/lib/types"
 import { createComment } from "@/lib/validation"
@@ -17,31 +17,47 @@ export const createCommentAction = async (input: {
     const session = await getServerSession()
     if (!session) throw new Error("Unauthorized")
 
-    const { content: contentValidated } = createComment.parse({ content: input.content })
-
-
-    const [newComment] = await db.insert(comments).values({
-        id: nanoid(),
-        content: contentValidated,
-        postId: input.post.id,
-        userId: session.user.id,
-    }).returning()
-
-
-    const [comment] = await db.select({
-        id: comments.id,
-        content: comments.content,
-        createdAt: comments.createdAt,
-        updatedAt: comments.updatedAt,
-        isEdited: comments.isEdited,
-
-        author: userFromDB(session.user.id),
+    const { content: contentValidated } = createComment.parse({
+        content: input.content
     })
-        .from(comments).innerJoin(user, eq(user.id, comments.userId))
-        .where(eq(comments.id, newComment.id))
-        .limit(1)
 
-    return comment;
+    const comment = await db.transaction(async (tx) => {
+
+        const [newComment] = await tx.insert(comments).values({
+            id: nanoid(),
+            content: contentValidated,
+            postId: input.post.id,
+            userId: session.user.id,
+        }).returning()
+
+        if (input.post.author.id !== session.user.id) {
+            await tx.insert(notification).values({
+                id: nanoid(),
+                issuerId: session.user.id,
+                recipientId: input.post.author.id,
+                postId: input.post.id,
+                type: "COMMENT",
+            })
+        }
+
+        const [comment] = await tx.select({
+            id: comments.id,
+            content: comments.content,
+            createdAt: comments.createdAt,
+            updatedAt: comments.updatedAt,
+            isEdited: comments.isEdited,
+
+            author: userFromDB(session.user.id),
+        })
+            .from(comments)
+            .innerJoin(user, eq(user.id, comments.userId))
+            .where(eq(comments.id, newComment.id))
+            .limit(1)
+
+        return comment
+    })
+
+    return comment
 }
 
 
@@ -49,9 +65,18 @@ export const deleteCommentActions = async (id: string) => {
     const session = await getServerSession()
     if (!session) throw new Error("Unauthorized")
 
-    const res = await db.delete(comments).where(and(eq(comments.id, id), eq(comments.userId, session.user.id))).returning()
-    if (!res) {
-        throw new Error("Post not found")
+    const res = await db.delete(comments)
+        .where(
+            and(
+                eq(comments.id, id),
+                eq(comments.userId, session.user.id)
+            )
+        )
+        .returning()
+
+    if (!res.length) {
+        throw new Error("Comment not found")
     }
-    return res[0];
-} 
+
+    return res[0]
+}
